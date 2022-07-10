43e01d92095649d487725b3b30b37ef58c2ce9b1
SantiagoRS26 <santiagors2611@gmail.com>
2022-07-10 12:51:32 -0500

    Modulos instalados

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include './layout/header.php';
+?>
+<main>
+    
+<!-- DESDE ACA PUEDEN EMPEZAR CON EL BODY -->
+
+</main>
+<?php
+include './layout/footer.php';
+?>
---
+++ layout/footer.php
@@ -0,0 +1,6 @@
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./node_modules/sweetalert2/dist/sweetalert2.min.js"></script>
+    <script src="./node_modules/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+</body>
+</html>
